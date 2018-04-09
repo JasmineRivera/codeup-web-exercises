9f4fa5f747fe53046bebd868b315cfe1deb0c536
jasminerivera <jazzyrivera@yahoo.com>
2018-04-09 02:32:42 -0500

    jquery events

--- js/jquery_js.js
+++ js/jquery_js.js
@@ -1,6 +1,42 @@
 'use strict';
 
-$(document).ready(function () {
-    alert('The DOM has finished loading!');
+// $(document).ready(function () {
+//     alert('The DOM has finished loading!');
+//
+// });
+//
+// // id selectors
+// // var contents = $('#list').html();
+// // alert(contents);
+// //
+// //
+// //
+// // var contents = $('#third').html();
+// // alert(contents);
+//
+//
+//
+// //class selectors
+//
+// // $('.codeup').css('border', '1px solid #F00');
+//
+//
+// // element selectors
+//
+// $('li').css('font-size', '20px');
+//
+// $('p, h1, li').css('background-color', 'pink');
 
-});
+
+
+//mouse events exercise below
+
+// $(document).ready(function () {
+//     alert('The DOM has finished loading!');
+//
+// });
+// var first = document.getElementsByTagName(h1);
+//
+// $(first).click(function () {
+//     alert('h1 was clicked');
+// });
